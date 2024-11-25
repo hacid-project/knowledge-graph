35670c72299101f73894db3347ec0a2bcefd11a2
Miguel Ceriani <miguel.ceriani@gmail.com>
2024-11-25 10:10:40 +0000

    Allow for selectively skipping RDF file uploads

--- data/climate-services/cmip/upload_kg.sh
+++ data/climate-services/cmip/upload_kg.sh
@@ -4,12 +4,16 @@ MAP_FILE_PATH="graph_names.csv"
 TDB2_LOADER=
 TDB2_DB_LOC="/opt/homebrew/var/fuseki/databases/hacid-test"
 
-tail -n +2 $MAP_FILE_PATH | awk -F "," '{ system("ls -l " $1 " | awk '\''{ print \"" $2 "\", $9 }'\''") }' | awk '
+tail -n +2 $MAP_FILE_PATH | awk -F "," '{
+        if ($3 != "y") {
+            system("ls -l " $1 " | awk '\''{ print \"" $2 "\", $9 }'\''")
+        }
+    }' | awk '
     BEGIN{
         OFS=","
     }
     {
-        a[$1]=(a[$1]?a[$1] FS:"")$2
+            a[$1] = (a[$1] ? a[$1] FS : "") $2
     }
     END{
         for(i in a){
