d450a8126aa46f96daf5fb2921f464d8b42ca638
Miguel Ceriani <miguel.ceriani@gmail.com>
2025-03-25 09:59:35 +0100

    Fix ontology integration KG reset

--- ontologies/integrate/reset_kg.sh
+++ ontologies/integrate/reset_kg.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 CONF_FILE_PATH="integrate/fuseki-conf.ttl"
+FUSEKI_CONF_PATH="/opt/homebrew/var/fuseki/configuration/"
 FUSEKI_DB_PATH="/opt/homebrew/var/fuseki/databases/"
 DS_NAME="hacid-onto-integrate"
 
